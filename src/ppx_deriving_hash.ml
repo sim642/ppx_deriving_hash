c5e64f0534c1c7309c49784f1448dda676bac1fd
Simmo Saan <simmo.saan@gmail.com>
2022-01-18 11:44:52 +0200

    Add quoter

--- src/ppx_deriving_hash.ml
+++ src/ppx_deriving_hash.ml
@@ -5,11 +5,12 @@ let mangle_affix = `Prefix "hash"
 
 let attr_hash = Attribute.declare "deriving.hash.hash" Attribute.Context.core_type Ast_pattern.(single_expr_payload __) Fun.id
 
-let rec expr ~loc ct =
+let rec expr ~loc ~quoter ct =
   match Attribute.get attr_hash ct with
   | Some hash ->
-    hash
+    Ppx_deriving.quote ~quoter hash
   | None ->
+    let expr = expr ~quoter in
     match ct with
     | [%type: string] ->
       [%expr Hashtbl.hash]
@@ -34,6 +35,7 @@ let rec expr ~loc ct =
       [%expr List.fold_left (fun a b -> 31 * a + [%e expr ~loc a] b) 0]
     | {ptyp_desc = Ptyp_constr ({txt = lid; loc}, args); _} ->
       let ident = pexp_ident ~loc {loc; txt = Ppx_deriving.mangle_lid mangle_affix lid} in
+      let ident = Ppx_deriving.quote ~quoter ident in
       let apply_args =
         args
         |> List.map (fun ct ->
@@ -42,15 +44,15 @@ let rec expr ~loc ct =
       in
       pexp_apply ~loc ident apply_args
     | {ptyp_desc = Ptyp_tuple comps; _} ->
-      expr_tuple ~loc comps
+      expr_tuple ~loc ~quoter comps
     | {ptyp_desc = Ptyp_variant (rows, Closed, None); _} ->
-      expr_poly_variant ~loc rows
+      expr_poly_variant ~loc ~quoter rows
     | {ptyp_desc = Ptyp_var name; _} ->
       evar ~loc ("poly_" ^ name)
     | _ ->
       Location.raise_errorf ~loc "other"
 
-and expr_poly_variant ~loc rows =
+and expr_poly_variant ~loc ~quoter rows =
   rows
   |> List.map (fun {prf_desc; _} ->
       match prf_desc with
@@ -59,7 +61,7 @@ and expr_poly_variant ~loc rows =
           ~guard:None
           ~rhs:([%expr 31])
       | Rtag ({txt = label; loc}, false, [ct]) ->
-        let label_fun = expr ~loc ct in
+        let label_fun = expr ~loc ~quoter ct in
         case ~lhs:(ppat_variant ~loc label (Some [%pat? x]))
           ~guard:None
           ~rhs:([%expr [%e label_fun] x])
@@ -68,7 +70,7 @@ and expr_poly_variant ~loc rows =
     )
   |> pexp_function ~loc
 
-and expr_variant ~loc constrs =
+and expr_variant ~loc ~quoter constrs =
   constrs
   |> List.map (fun {pcd_name = {txt = label; loc}; pcd_args; pcd_res; _} ->
       match pcd_res, pcd_args with
@@ -84,7 +86,7 @@ and expr_variant ~loc constrs =
         let body =
           cts
           |> List.mapi (fun i comp_type ->
-              (i, expr ~loc comp_type)
+              (i, expr ~loc ~quoter comp_type)
             )
           |> List.map (fun (i, label_fun) ->
               [%expr [%e label_fun] [%e label_field ~loc "x" i]]
@@ -108,14 +110,14 @@ and expr_variant ~loc constrs =
     )
   |> pexp_function ~loc
 
-and expr_record ~loc lds =
+and expr_record ~loc ~quoter lds =
   let label_field ~loc record_expr label =
     pexp_field ~loc record_expr {loc; txt = Lident label}
   in
   let body x_expr =
     lds
     |> List.map (fun {pld_name = {txt = label; loc}; pld_type; _} ->
-        (label, expr ~loc pld_type)
+        (label, expr ~loc ~quoter pld_type)
       )
     |> List.map (fun (label, label_fun) ->
         [%expr [%e label_fun] [%e label_field ~loc x_expr label]]
@@ -124,7 +126,7 @@ and expr_record ~loc lds =
   in
   [%expr fun x -> [%e body [%expr x]]]
 
-and expr_tuple ~loc comps =
+and expr_tuple ~loc ~quoter comps =
   let label_field ~loc prefix i =
     let name = prefix ^ string_of_int i in
     pexp_ident ~loc {loc; txt = Lident name}
@@ -132,7 +134,7 @@ and expr_tuple ~loc comps =
   let body =
     comps
     |> List.mapi (fun i comp_type ->
-        (i, expr ~loc comp_type)
+        (i, expr ~loc ~quoter comp_type)
       )
     |> List.map (fun (i, label_fun) ->
         [%expr [%e label_fun] [%e label_field ~loc "x" i]]
@@ -149,23 +151,25 @@ and expr_tuple ~loc comps =
   in
   [%expr fun [%p pat "x"] -> [%e body]]
 
-let expr_declaration ~loc td = match td with
+let expr_declaration ~loc ~quoter td = match td with
   | {ptype_kind = Ptype_abstract; ptype_manifest = Some ct; _} ->
-    expr ~loc ct
+    expr ~loc ~quoter ct
   | {ptype_kind = Ptype_abstract; _} ->
     Location.raise_errorf ~loc "Cannot derive accessors for abstract types"
   | {ptype_kind = Ptype_variant constrs; _} ->
-    expr_variant ~loc constrs
+    expr_variant ~loc ~quoter constrs
   | {ptype_kind = Ptype_open; _} ->
     Location.raise_errorf ~loc "Cannot derive accessors for open types"
   | {ptype_kind = Ptype_record fields; _} ->
-    expr_record ~loc fields
+    expr_record ~loc ~quoter fields
 
 let generate_impl ~ctxt (_rec_flag, type_declarations) =
   let loc = Expansion_context.Deriver.derived_item_loc ctxt in
   type_declarations
   |> List.map (fun td ->
-      let expr = expr_declaration ~loc td in
+      let quoter = Ppx_deriving.create_quoter () in
+      let expr = expr_declaration ~loc ~quoter td in
+      let expr = Ppx_deriving.sanitize ~quoter expr in
       let expr = Ppx_deriving.poly_fun_of_type_decl td expr in
       let pat = ppat_var ~loc {loc; txt = Ppx_deriving.mangle_type_decl mangle_affix td} in
       Ast_helper.Vb.mk ~loc pat expr
@@ -190,7 +194,9 @@ let generate_intf ~ctxt (_rec_flag, type_declarations): signature_item list =
 
 let intf_generator = Deriving.Generator.V2.make_noarg generate_intf
 
-let extension ~loc ~path:_ ct = expr ~loc ct
+let extension ~loc ~path:_ ct =
+  let quoter = Ppx_deriving.create_quoter () in
+  Ppx_deriving.sanitize ~quoter (expr ~loc ~quoter ct)
 
 let my_deriver =
   Deriving.add
