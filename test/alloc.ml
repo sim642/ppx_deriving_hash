63ed5c22fc00cb30e80898cec9b2638f8c8d1717
Simmo Saan <simmo.saan@gmail.com>
2026-07-20 22:06:03 +0300

    Disable some allocation tests on OCaml < 5.2.0

--- test/alloc.ml
+++ test/alloc.ml
@@ -5,5 +5,7 @@ open Types
 
 let tests =
   "alloc" >::: [
+#if OCAML_VERSION < (5, 2, 0)
     "list2" >:: test_no_alloc (hash_poly2 (fun x -> x) (fun x -> Char.code x)) (Cons2 (1, 'a', Cons2 (2, 'b', Cons2 (3, 'c', Nil2))));
+#endif
   ]
--- test/giltho/alloc.ml
+++ test/giltho/alloc.ml
@@ -10,12 +10,16 @@ let t = { node = TBranch ({ node = TLeaf 1; tag = 1 }, { node = TLeaf 2; tag = 2
 let tests =
   "giltho" >::: [
     "alloc" >::: [
+#if OCAML_VERSION < (5, 2, 0)
       "expr" >:: test_no_alloc hash_expr e;
+#endif
       "nested" >:: test_no_alloc hash_nested [| [ [ 1; 2; 3 ]; [ 4; 5 ] ]; [ [ 6 ] ] |];
       "rgb" >:: test_no_alloc hash_rgb { r = 1; g = 2; b = 3 };
       "pair" >:: test_no_alloc hash_pair (4, 5);
       "tree" >:: test_no_alloc hash_tree t;
+#if OCAML_VERSION < (5, 2, 0)
       "list" >:: test_no_alloc (hash_poly (fun x -> x)) (Cons (1, Cons (2, Cons (3, Nil))));
+#endif
       (* primitives mixed (incl. a float field, which must not re-box on read) *)
       "prim" >:: test_no_alloc hash_prim { i = 1; b = true; c = 'a'; s = "x"; f = 3.5 };
       "option" >:: test_no_alloc hash_opt (Some 5);
@@ -25,7 +29,9 @@ let tests =
       "inline record" >:: test_no_alloc hash_inline (Inl { x = 1; y = "a" });
       "two params" >:: test_no_alloc (hash_two (fun x -> x) Char.code) (Two (1, 'a'));
       "pairs" >:: test_no_alloc hash_pairs [ (1, 2); (3, 4); (5, 6) ];
+#if OCAML_VERSION < (5, 2, 0)
       "rgb array" >:: test_no_alloc hash_rgb_arr [| { r = 1; g = 2; b = 3 } |];
       "override in list" >:: test_no_alloc hash_wrapped_list [ 10; 20; 30 ];
+#endif
     ];
   ]
